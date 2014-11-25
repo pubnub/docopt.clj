06e57ea3e621fa984b6e530be53b4cbd32ef4888
Harlan Iverson <harlan@pubnub.com>
2014-11-25 15:54:47 -0800

    get credentials for deployment from CLI

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "MIT" :url "https://github.com/docopt/docopt.clj/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}}
-  :aot :all)
+  :aot :all
+  :plugins [[s3-wagon-private "1.1.2"]]  
+  :repositories [["private" {:url "s3p://maven-repo-rafta/snapshot/"
+                             :username ~(System/getenv "LEIN_USERNAME")
+                             :passphrase ~(System/getenv "LEIN_PASSPHRASE")}]]
+  )
