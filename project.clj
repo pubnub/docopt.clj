ba8ff64bfc178a922cfc15f8001996ada488399c
Harlan Iverson <harlan@pubnub.com>
2014-12-08 13:51:50 -0800

    updated repos for new maven internal

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}}
   :aot :all
-  :plugins [[s3-wagon-private "1.1.2"]]  
-  :repositories [["private" {:url "s3p://maven-repo-rafta/snapshot/"
-                             :username ~(System/getenv "LEIN_USERNAME")
-                             :passphrase ~(System/getenv "LEIN_PASSPHRASE")}]]
-  )
+ 
+  :mirrors {#".+" {:name "pubnub" :url "http://maven.devbuild.pubnub.com:8081/artifactory/repo"}} 
+  :deploy-repositories [["releases" {:url "http://maven.devbuild.pubnub.com:8081/artifactory/ext-release-local"}]
+                        ["snapshots" {:url "http://maven.devbuild.pubnub.com:8081/artifactory/ext-snapshot-local"}]])
